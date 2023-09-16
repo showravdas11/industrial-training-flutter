bbd411a32e89daeab42405b0e87dc1f4f68b144d
showravdas11 <showravdas11@gmail.com>
2023-09-17 02:29:26 +0600

    work done

--- lib/main.dart
+++ lib/main.dart
@@ -24,11 +24,35 @@ class MyApp extends StatelessWidget {
           ),
         ),
         body: Center(
-            child: Container(
-          width: 400,
-          height: 400,
-          child: Image.asset('assets/images/naruto.jpg'),
-        )),
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: 400,
+                height: 400,
+                child: Image.asset('assets/images/naruto.jpg'),
+              ),
+              Text(
+                "Hi i'm Naruto Uzumaki and It's my team",
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700),
+              ),
+              Padding(padding: EdgeInsets.only(top: 10.0, bottom: 10.0)),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(20.0),
+                  backgroundColor: Colors.black, // background (button) color
+                  foregroundColor: Colors.white, // foreground (text) color
+                ),
+                child: Text('Watch Now'),
+                onPressed: () {
+                  print("watched");
+                },
+              )
+            ],
+          ),
+        ),
 
         // It's a bottomNavigation bar
         bottomNavigationBar: BottomNavigationBar(
